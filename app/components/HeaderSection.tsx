83d392eac3b4061bfec4b052df8430e4df29ab02
Eric5603 <ericwanyoike123@gmail.com>
2025-01-28 22:05:35 +0300

    remove all

--- app/components/HeaderSection.tsx
+++ app/components/HeaderSection.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
+
 
 const HeaderSection: React.FC = () => {
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
