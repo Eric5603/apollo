997104b7c023b533495adbf2ab27da1f145a8d5e
Eric5603 <ericwanyoike123@gmail.com>
2025-01-29 10:47:44 +0300

    more changes on the app

--- app/components/FeatureSection.tsx
+++ app/components/FeatureSection.tsx
@@ -23,7 +23,7 @@ const FeatureSection: React.FC = () => {
     {
       id: 2,
       title: 'Commercial Office Tower',
-      image: '/images/Features/Features4.jpg',
+      image: '/images/Features/Features5.jpg',
       description: 'State-of-the-art corporate hub featuring smart building technology',
       category: 'Commercial'
     },
